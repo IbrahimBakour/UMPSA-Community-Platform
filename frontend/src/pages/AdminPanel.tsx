3767f1b8620ce6e985e8865362847230e393d4a8
IbrahimBakour <68356802+IbrahimBakour@users.noreply.github.com>
2025-10-30 20:37:38 +0800

    Removed unused import

--- frontend/src/pages/AdminPanel.tsx
+++ frontend/src/pages/AdminPanel.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Link, Outlet, useLocation } from 'react-router-dom';
 import TopNavigation from '../components/layout/TopNavigation';
 import Sidebar from '../components/layout/Sidebar';
